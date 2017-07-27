20da0e65663bab0d674c9dffb395729509a7a87a
Todd <tpolasek@gmail.com>
2017-07-27 15:03:51 -0700

    Added post creation and routing

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,48 +1,44 @@
-import React, {Component} from 'react';
-import {fetchPosts} from '../actions';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
-import _ from 'lodash';
+import React, { Component } from "react";
+import { fetchPosts } from "../actions";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import _ from "lodash";
 
 class PostsIndex extends Component {
-		componentDidMount() {
-				this
-						.props
-						.fetchPosts();
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
 
-		}
-
-		renderPosts() {
-				return _.map(this.props.posts, post => {
-						if (post.title) {
-								return (
-										<li className="list-group-item" key={post.id}>
-												{post.title}
-										</li>
-								);
-						}
-
-				});
-		}
-		render() {
-				return (
-						<div>
-							<div className="text-xs-right">
-								<Link className="btn btn-primary" to="/posts/new">
-									Add a Post
-								</Link>
-								</div>
-								<h3>Posts</h3>
-								<ul className="list-group">
-										{this.renderPosts()}
-								</ul>
-						</div>
-				);
-		}
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      if (post.title) {
+        return (
+          <li className="list-group-item" key={post.id}>
+            {post.title}
+          </li>
+        );
+      }
+    });
+  }
+  render() {
+    return (
+      <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-		return {posts: state.posts}
+  return { posts: state.posts };
 }
 
-export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
